d534fcc7d2389d08db419c66533fbe81fe3ccbfb
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-06 14:59:04 +0100

    correction fichier javascript

--- 2-les-variables-types-de-données/exercice-2/app.js
+++ 2-les-variables-types-de-données/exercice-2/app.js
@@ -1,5 +1,5 @@
 const motDePasse = 1965;
 const userName = "Franck";
 const dateAnniversaire = 150566;
-const prixDuTicket = 23.95 ;
-const jeSuisUneVariableTresLongueAModifierEnCamelCase = OK;
+const prixDuTicket = 23.95;
+const jeSuisUneVariableTresLongueAModifierEnCamelCase = "OK";
