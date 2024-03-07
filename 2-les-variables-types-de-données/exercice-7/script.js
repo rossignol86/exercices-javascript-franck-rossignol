ba2b7bb952370665352d392893e96c427f360511
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-07 13:26:32 +0100

    script exercice7

--- 2-les-variables-types-de-données/exercice-7/script.js
+++ 2-les-variables-types-de-données/exercice-7/script.js
@@ -1,3 +1,6 @@
+// let somme = 10 * 2;
+// console.log(somme);
+
 // Résultat attendu pour 10 et 2, dans la console :
 // 10 x 2 = 20;
 // 10 - 2 = 8;
@@ -5,5 +8,11 @@
 // 10 / 2 = 5;
 // 10 exposant 2 = 100;
 
-let chiffre1 = prompt ('Donner moi 1 chiffre');
-let chiffre2
+
+let a = prompt("premier chiffre");
+a = parseInt(a)
+let b = prompt("deuxieme chiffre");
+b = parseInt(b)
+
+let somme = a + b;
+console.log(somme);
