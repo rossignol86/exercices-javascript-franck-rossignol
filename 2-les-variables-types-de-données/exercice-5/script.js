5081e0fbb6a511c9c6780a483d5c2a05ca1ea0a6
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-07 11:09:02 +0100

    script exercice5

---
+++ 2-les-variables-types-de-données/exercice-5/script.js
@@ -0,0 +1,9 @@
+let age = 25;
+let genre = "femme";
+let estContent = true;
+
+// Résultat attendu dans la console : 
+
+// La variable "age" est de type : number
+// La variable "genre" est de type : string
+// La variable "estContent" est de type : boolean
