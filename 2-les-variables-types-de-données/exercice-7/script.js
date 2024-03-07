c8603d203a40fc7ba7ef67955c06d7cc278e9fc3
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-07 13:55:19 +0100

    rajout d'opération

--- 2-les-variables-types-de-données/exercice-7/script.js
+++ 2-les-variables-types-de-données/exercice-7/script.js
@@ -2,25 +2,34 @@
 // console.log(somme);
 
 // Résultat attendu pour 10 et 2, dans la console :
-// 10 x 2 = 20;
-// 10 - 2 = 8;
-// 10 + 2 = 12;
-// 10 / 2 = 5;
-// 10 exposant 2 = 100;
-
+// 10 x 2 = 20; fait
+// 10 - 2 = 8; fait
+// 10 + 2 = 12; fait
+// 10 / 2 = 5; fait
+// 10 exposant 2 = 100; fait
 
 let a = prompt("premier chiffre");
 a = parseInt(a)
 let b = prompt("deuxieme chiffre");
 b = parseInt(b)
 
+let quotient = a / b;
+console.log(quotient);
+let message4 =`${a} / ${b} = ${quotient}`;
+console.log(message4)
+
+let produit = a * b;
+console.log(produit);
+let message2 = `${a} x ${b} = ${produit}`;
+console.log(message2)
+
+let difference = a - b;
+console.log(difference);
+let message3 = `${a} - ${b} = ${difference}`
+console.log(message3)
+
 let somme = a + b;
 console.log(somme);
+let message1 = `${a} + ${b} = ${somme}`;
+console.log(message1)
 
-let message = `${a} + ${b} = ${somme}`;
-console.log(message)
-
-
-
-// let produit= a * b;
-// console.log(produit);
