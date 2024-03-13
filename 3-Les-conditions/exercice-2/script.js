e2d398e89d2500589ef6a2682841f08ff548390d
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-13 11:48:12 +0100

    modification

---
+++ 3-Les-conditions/exercice-2/script.js
@@ -0,0 +1,23 @@
+// Exemples de résultats attendus dans la console :
+
+// Le nombre 1 est plus grand que le nombre 2
+
+// Le nombre 1 est plus petit que le nombre 2
+
+// Les deux nombres sont égaux
+
+// let nombre1 = 150;
+// let nombre2 = 100;
+
+let nombre1 = prompt("premier chiffre");
+let nombre2 = prompt("deuxieme chiffre");
+
+if (nombre1 > nombre2) {
+    console.log("le nombre 1 est plus grand !");
+} else if (nombre1 < nombre2) {
+    console.log("le nombre 1 est plus petit !");
+} else {
+    console.log("les deux nombre sont égaux !");
+}
+
+
