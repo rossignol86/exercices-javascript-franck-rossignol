27ce22e86b581b264d9c98e98bd0ca81fdd01cdf
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-13 14:18:28 +0100

    creation

--- 3-Les-conditions/exercice-5/script.js
+++ 3-Les-conditions/exercice-5/script.js
@@ -1,18 +1,7 @@
-// Exemples de résultats attendus :
-
-// si le mot de passe est correct :
-
-// OK 
-
-// si le mot de passe est incorrect
-
-// Accès interdit
-
-let motDePasse = prompt("Mot de passe");
-let motDePassePerso = `1234ABC`;
-
-if (motDePasse === motDePassePerso) {
-    console.log("Mot de passe valide");
-} else if (motDePasse !== motDePassePerso) {
-    console.log("Mot de passe invalide");
-}
+// Résultat attendu dans la console :
+// pour une note < 12 'Pas de mention'
+// Pour 12 et + 'Passable'
+// Pour 14 et + 'Bien'
+// Pour 16 et + 'Très bien'
+// Pour 18 et + 'Excellent'
+// Pour 20 'Extraordinaire'
