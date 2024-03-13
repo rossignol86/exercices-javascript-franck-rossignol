9ea58180067a1f1e9857f43090d553c084ff4ec2
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-13 10:55:04 +0100

    modification

--- 3-Les-conditions/exercice-1/script.js
+++ 3-Les-conditions/exercice-1/script.js
@@ -1,9 +1,11 @@
 // Résultat attendu dans la console, si l'âge < 18 :
 // Vous êtes mineur
 
-let age = 18;
+let age = 28;
 if (age <= 18) {
     console.log ("vous êtes mineur !");
+} else {
+    console.log("vous etes majeur !")
 }
 
 
