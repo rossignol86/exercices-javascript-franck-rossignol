2558d8cb7eda9a403684b1460c8c8d88c9528377
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-14 14:18:23 +0100

    modification

---
+++ 4-les-boucles/exercice-7/script.js
@@ -0,0 +1,12 @@
+// Résultat attendu dans la console :
+//
+// 7 x 1 = 7
+// 7 x 2 = 14
+// ...
+// 7 x 10 = 70
+
+
+for (let i = 1; i <= 10; i++) {
+    let total = 7 * i;
+    console.log("7 x " + 1 + " = " + total); 
+}
