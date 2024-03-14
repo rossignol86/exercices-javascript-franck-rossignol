91f5b9042e3f029a6aca4d28e2ba55c793ce8b16
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-03-14 10:11:24 +0100

    modification

---
+++ 4-les-boucles/exercice-1/script.js
@@ -0,0 +1,13 @@
+// Résultat attendu dans la console :
+// 0
+// 1
+// 2
+// ...
+// 10
+
+let compteur = 0;
+
+while (compteur < 11) {
+    console.log(compteur);
+    compteur++;
+}
---
+++ 4-les-boucles/exercice-3/script.js
@@ -0,0 +1,7 @@
+let compteur = 5;
+
+ while (compteur > 0) {
+    console.log(compteur);
+    compteur--;
+ }
+ console.log("Décollage 🚀");
