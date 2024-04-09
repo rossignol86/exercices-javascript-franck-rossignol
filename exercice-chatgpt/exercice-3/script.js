2965c6326ebc7e0ad981b2b2aefd539ece16b482
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-09 09:12:05 +0200

    modif

--- exercice-chatgpt/exercice-3/script.js
+++ exercice-chatgpt/exercice-3/script.js
@@ -2,10 +2,12 @@ document.getElementById("blurButton").addEventListener("click", function() {
     var cells = document.querySelectorAll(".cell img");
     cells.forEach(function(cell) {
         cell.style.filter = "blur(5px)";
+        cell.style.border = 'none';
     });
     setTimeout(function() {
         cells.forEach(function(cell) {
             cell.style.filter = "none";
         });
     }, 2000);
+    
 });
