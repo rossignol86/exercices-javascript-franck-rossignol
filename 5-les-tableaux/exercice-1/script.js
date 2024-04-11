e5100e54b9343277aa64f304b31e69bc9f2a7eff
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-11 10:20:17 +0200

    modif

---
+++ 5-les-tableaux/exercice-1/script.js
@@ -0,0 +1,17 @@
+// Résultat attendu dans la console :
+// 0
+// 1
+// 2
+// ...
+// 10
+
+// Preferez cette methode pour ce cas
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// let compteur = 0;
+// while (compteur <= 10) {
+//     console.log(compteur);
+//     compteur++;
+// }
