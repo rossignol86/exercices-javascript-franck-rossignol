fbbce32dd4d7345b6d30a7bd75a168fd4ed1ceb6
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-11 11:06:44 +0200

    corrections

--- 5-les-tableaux/exercice-1/script.js
+++ 5-les-tableaux/exercice-1/script.js
@@ -19,20 +19,3 @@ console.log(nombres[nombres.length - 1]);
 console.log(`le premier nombre du tableau est : ${nombres[0]}`);
 
 console.log(`le troisième nombre du tableau est : ${nombres[2]}`);
-//const nombres = [12, 18, 33, 5, 78, 56, 47, 28]
-
-// Résultat dans la console :
-
-//le premier nombre du tableau est 12
-//le troisième nombre du tableau est 33
-//le dernier nombre du tableau est 28
-
-let nombres = [12, 18, 33, 5, 78, 56, 47, 28]
-console.table(nombres);
-
-
-console.log(nombres[0]);
-
-console.log(nombres[2]);
-
-console.log(nombres[nombres.length - 1]);
