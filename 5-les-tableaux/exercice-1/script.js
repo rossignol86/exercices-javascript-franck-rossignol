5f2981dfe7e22e0a73c8edb1ab6ad6d07c7502ae
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-11 11:06:35 +0200

    corrections

--- 5-les-tableaux/exercice-1/script.js
+++ 5-les-tableaux/exercice-1/script.js
@@ -1,17 +1,38 @@
-// Résultat attendu dans la console :
-// 0
-// 1
-// 2
-// ...
-// 10
+//const nombres = [12, 18, 33, 5, 78, 56, 47, 28]
 
-// Preferez cette methode pour ce cas
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
-}
+// Résultat dans la console :
 
-// let compteur = 0;
-// while (compteur <= 10) {
-//     console.log(compteur);
-//     compteur++;
-// }
+//le premier nombre du tableau est 12
+//le troisième nombre du tableau est 33
+//le dernier nombre du tableau est 28
+
+let nombres = [12, 18, 33, 5, 78, 56, 47, 28]
+console.table(nombres);
+
+
+console.log(nombres[0]);
+
+console.log(nombres[2]);
+
+console.log(nombres[nombres.length - 1]);
+
+console.log(`le premier nombre du tableau est : ${nombres[0]}`);
+
+console.log(`le troisième nombre du tableau est : ${nombres[2]}`);
+//const nombres = [12, 18, 33, 5, 78, 56, 47, 28]
+
+// Résultat dans la console :
+
+//le premier nombre du tableau est 12
+//le troisième nombre du tableau est 33
+//le dernier nombre du tableau est 28
+
+let nombres = [12, 18, 33, 5, 78, 56, 47, 28]
+console.table(nombres);
+
+
+console.log(nombres[0]);
+
+console.log(nombres[2]);
+
+console.log(nombres[nombres.length - 1]);
