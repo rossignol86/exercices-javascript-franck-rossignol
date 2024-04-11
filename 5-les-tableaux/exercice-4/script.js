ef41f530c3f3de3c9ef3935f5c5a7eaadb486993
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-11 14:04:09 +0200

    modif

--- 5-les-tableaux/exercice-4/script.js
+++ 5-les-tableaux/exercice-4/script.js
@@ -1,17 +1,38 @@
-// Résultat attendu dans la console :
-// 0
-// 1
-// 2
-// ...
-// 10
+// À l'aide d'une boucle for, réalisez les tâches suivantes :
+// Remplissez un tableau vide avec les 10 premiers nombres, puis affichez
+//"Les dix premiers nombres sont : 1, 2, 3, 4, 5, 6, 7, 8, 9, 10".
+//2. Remplissez un deuxième tableau vide avec les 10 premiers nombres pairs,
+//puis affichez "Les dix premiers nombres pairs sont : 2, 4, 6, 8, 10, 12, 14, 16, 18, 20".
 
-// Preferez cette methode pour ce cas
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
+let tableau = [ ];
+console.table(tableau);
+
+for (let i = 1; i < 11; i++) {
+// i dans le tableau
+    tableau.push(i);
+ //console.log(i);
 }
+console.log(tableau)
+console.log(`Les dix premiers nombres sont : ${tableau}`);
 
-// let compteur = 0;
-// while (compteur <= 10) {
-//     console.log(compteur);
-//     compteur++;
-// }
+
+
+
+let tableauPaire = [ ];
+console.table(tableauPaire);
+for (var i = 2; i <= 20; i += 2) {
+    tableauPaire.push(i);
+}
+console.log(tableauPaire)
+console.log(`Les dix premiers nombres pairs sont : ${tableauPaire}`);
+
+
+
+
+let tableauImpaire = [ ];
+console.table(tableauImpaire);
+for (var i = 1; i <= 20; i += 2) {
+    tableauImpaire.push(i);
+}
+console.log(tableauImpaire)
+console.log(`Les dix premiers nombres impairs sont : ${tableauImpaire}`);
