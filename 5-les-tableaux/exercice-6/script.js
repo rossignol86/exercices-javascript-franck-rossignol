80c4fa11575636e6267c261cb1258af3bfce7c19
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-12 09:57:48 +0200

    nouveaux

--- 5-les-tableaux/exercice-6/script.js
+++ 5-les-tableaux/exercice-6/script.js
@@ -1,17 +1,29 @@
 // Résultat attendu dans la console :
-// 0
-// 1
-// 2
-// ...
-// 10
 
-// Preferez cette methode pour ce cas
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
-}
 
-// let compteur = 0;
-// while (compteur <= 10) {
-//     console.log(compteur);
-//     compteur++;
-// }
+
+let mots = [
+	"arbre", 
+	"soleil", 
+	"lumière", 
+	"pomme", 
+	"ordinateur", 
+	"livre", 
+	"bouteille", 
+	"téléphone", 
+	"fenêtre", 
+	"chaise", 
+	"table", 
+	"voiture", 
+	"ciel", 
+	"eau", 
+	"ordinateur", 
+	"souris", 
+	"clavier", 
+	"écran", 
+	"lampe", 
+	"bureau"
+];
+
+let petitsMots = [];
+let grandsMots = [];
