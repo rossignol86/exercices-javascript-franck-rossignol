d07e5ef1cdbc8607cd70a23fba3bab93508cfa9a
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-17 13:50:45 +0200

    correction

--- 7-manipuler-donnees/exercice-1/script.js
+++ 7-manipuler-donnees/exercice-1/script.js
@@ -4,3 +4,24 @@
 // dans un ordre différent.
 // La fonction doit retourner un booléen : true
 // si les mots sont des anagrammes, false sinon
+
+function sontAnagrammes(mot1, mot2) {
+    mot1 = mot1.split(``) //divise caratere en tableau
+    mot1.sort(); //trie les elements du tableau
+    mot1 = mot1.join(``) //joint les elements du tableau
+
+    mot2 = mot2.split(``) //divise caratere en tableau
+    mot2.sort(); //trie les elements du tableau
+    mot2 = mot2.join(``) //joint les elements du tableau
+
+    if (mot1 === mot2) {  //si mot1 est egal à mot2
+        return true //retourne vrai
+    } else { //sinon
+        return false //retourne faux
+    } 
+}
+
+console.log(sontAnagrammes('toto', 'titi'))
+console.log(sontAnagrammes('chien', 'niche'))
+console.log(sontAnagrammes('toto', 'titi'))
+
