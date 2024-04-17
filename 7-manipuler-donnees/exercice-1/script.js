e0d8c142794e9cf0f6282f7a669460ceb9bf9d8e
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-17 11:48:29 +0200

    correction

--- 7-manipuler-donnees/exercice-1/script.js
+++ 7-manipuler-donnees/exercice-1/script.js
@@ -1,8 +1,14 @@
-//Créez un fonction qui affiche une présentation
-//de vous, dans la console. 
+// Créez une fonction qui prend deux mots en paramètre
+// et vérifie s'ils sont des anagrammes,
+// c'est-à-dire s'ils contiennent les mêmes lettres
+// dans un ordre différent.
+// La fonction doit retourner un booléen : true
+// si les mots sont des anagrammes, false sinon
 
-function description() {
-    console.log("je m'appelle Franck");
-}
 
-description()
+function anagrammes() {
+    return "Bonjour tout le monde !";
+  }
+  
+  const resultat = sayHello();
+  console.log(resultat);
