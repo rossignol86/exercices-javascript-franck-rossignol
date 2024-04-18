ac296c74706ce5e897e452b563b33a81f0d9f768
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-18 13:43:16 +0200

    correction

--- 8 - La gestion du DOM/exercice-2/script.js
+++ 8 - La gestion du DOM/exercice-2/script.js
@@ -1,17 +1,26 @@
 // Dans votre HTML :
 // 1 - creer un header (element HTML)
 // - creer un element footer (element HTML)
-console.log("coucou")
+//console.log("coucou")
 
 
 // Dans votre Javascript :
 // 2 - creer un element html h1
-let nouvelElement = document.createElement
+let nouveauTitreH1 = document.createElement("h1");
 
 // 3 - modifier la taille de votre h1 (50px)
-// 4 - changer la couleur du h1 
-// 5 - Ajouter un texte dans votre h1
-// 6 - Ajouter votre element H1 => dans votre element Header
-// 7 - Supprimer votre element footer
+nouveauTitreH1.style.fontSize = "50px";
 
-let nouvelElement
+// 4 - changer la couleur du h1
+nouveauTitreH1.style.color = "green";
+
+// 5 - Ajouter un texte dans votre h1
+nouveauTitreH1.innerText = "Nouveau titre";
+
+// 6 - Ajouter votre element H1 => dans votre element Header
+let baliseHeader = document.querySelector('header');
+baliseHeader.appendChild(nouveauTitreH1);
+
+// 7 - Supprimer votre element footer
+let baliseFooter = document.querySelector('footer');
+baliseFooter.remove();
