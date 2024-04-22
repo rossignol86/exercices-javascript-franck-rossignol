d0711056c1d7ad7d4453b98ebf512763fe057f26
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-22 09:03:58 +0200

    correction

--- 8 - La gestion du DOM/exercice-3/script.js
+++ 8 - La gestion du DOM/exercice-3/script.js
@@ -1,9 +1,17 @@
-// 
+// Créez un bouton qui, lorsqu'il est cliqué,
+//change la couleur de fond de la page (du <body>).
+//Centrez le bouton au milieu de la page
+//(horizontalement et verticalement).
 
 console.log("salut l'artiste");
 
-let couleurFond = document.querySelector('body');
+let button = document.querySelector('.changecouleur');
 
+
+button.addEventListener('click', function () {
+    let body = document.querySelector('body')
+    body.style.background = 'black'
+})
 
 
 
