e90f12db835d0f0203e3606bcd926d982d1db13d
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-24 10:41:15 +0200

    modification

--- 8 - La gestion du DOM/exercice-1/script.js
+++ 8 - La gestion du DOM/exercice-1/script.js
@@ -1,8 +1,11 @@
+//En javascript,
+//changez le texte du <h1> en “Bonjour le Monde”
+
 console.log("coucou")
 
 let elementA = document.querySelector('a');
 console.log(elementA)
 
-elementA.style.backgroundColor = "blue";
+//elementA.style.backgroundColor = "blue";
 
 elementA.innerText ="nouveau text"
