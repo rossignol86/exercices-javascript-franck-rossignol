cc8967ca57c6e7d318f78db39f20607754b70c63
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-24 13:01:13 +0200

    modification

--- 8 - La gestion du DOM/exercice-4/script.js
+++ 8 - La gestion du DOM/exercice-4/script.js
@@ -5,5 +5,14 @@
 //les ajouter au DOM.
 
 function createTag(tagName, content) {
-    console.log("Bonjour tout le monde !");
+    const element = document.createElement(tagName);
+    element.innerText = content;
+    return element;
 }
+
+
+//Test de la fonction en créant un paragraphe et un titre et en les ajoutant au DOM
+const paragraph = createTag('p', 'Ceci est un paragraphe créé avec JavaScript.');
+const heading = createTag('h1', 'Titre créé dynamiquement');
+
+console.log(paragraph, heading);
--- 8 - La gestion du DOM/exercice-5/script.js
+++ 8 - La gestion du DOM/exercice-5/script.js
@@ -1,42 +1,11 @@
-// Créez une fonction qui prend deux mots en paramètre
-// et vérifie s'ils sont des anagrammes,
-// c'est-à-dire s'ils contiennent les mêmes lettres
-// dans un ordre différent.
-// La fonction doit retourner un booléen : true
-// si les mots sont des anagrammes, false sinon
+// Affichez une image sur votre page.
+//Ajoutez un évènement pour qu’au survol de cette image,
 
+let element = document.querySelector('img')
 
-function sontAnagrammes(mot1, mot2) {
-    mot1 = mot1.split(``) //divise caratere en tableau
-    mot1.sort(); //trie les elements du tableau
-    mot1 = mot1.join(``) //joint les elements du tableau
+element.addEventListener('survol', function(mousemove) {
+    console.log('Élément survolé');
+});
 
-    mot2 = mot2.split(``) //divise caratere en tableau
-    mot2.sort(); //trie les elements du tableau
-    mot2 = mot2.join(``) //joint les elements du tableau
-
-    if (mot1 === mot2) {  //si mot1 est egal à mot2
-        return true //retourne vrai
-    } else { //sinon
-        return false //retourne faux
-    } 
-}
-
-console.log(sontAnagrammes('toto', 'titi'))
-console.log(sontAnagrammes('chien', 'niche'))
-console.log(sontAnagrammes('toto', 'titi'))
-
-
-
-function sontAnagrammes(motA, motB) {
-    motA = motA.split(``).sort().join(``)
-    motB = motB.split(``).sort().join(``)
-
-    if (motA === motB) {  //si motA est egal à motB
-        return true //retourne vrai
-    } else { //sinon
-        return false //retourne faux
-    } 
-}
-
-console.log(sontAnagrammes('chine', 'niche'))
+//elle devienne floue.
+//(faite le bien en javascript, pas avec un :hover)
