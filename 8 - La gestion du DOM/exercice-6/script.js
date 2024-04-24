9fbf343ff2f39b5bcc1f27c5f9dd744e43a7db6f
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-24 14:39:49 +0200

    corrections

--- 8 - La gestion du DOM/exercice-6/script.js
+++ 8 - La gestion du DOM/exercice-6/script.js
@@ -1,11 +1,25 @@
-//En javascript,
-//changez le texte du <h1> en “Bonjour le Monde”
+//À partir d'un tableau d'images,
+//créez une liste d'images sur votre page. 
 
-console.log("coucou")
+//Assurez-vous que chaque image ait une taille
+//de 300px par 300px sans être déformée. 
 
-let elementA = document.querySelector('a');
-console.log(elementA)
+//Utilisez Flexbox pour organiser les images
+//de manière esthétique sur la page.
 
-//elementA.style.backgroundColor = "blue";
+const images = [
+    "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1524413840807-0c3cb6fa808d?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1528164344705-47542687000d?q=80&w=3584&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1661371927364-e3aec9079c66?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1490806843957-31f4c9a91c65?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1492571350019-22de08371fd3?q=80&w=3453&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1480796927426-f609979314bd?q=80&w=2300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+]
 
-elementA.innerText ="nouveau text"
+for (const image of images){
+    let nouvelElement = document.createElement('img');
+    document.body.appendChild(nouvelElement);
+   
+    console.log (image)
+}
