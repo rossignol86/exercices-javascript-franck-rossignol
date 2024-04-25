cbddcc1476a326e4fee256fbba8a7d169427caba
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-04-25 10:23:50 +0200

    corrections

---
+++ 1-introduction-au-javascript/exercice-1/script.js
@@ -0,0 +1,28 @@
+// Fichier: script.js
+
+console.log('Hello World');
+
+//Permet d’écrire directement sur la page web. 
+document.write("hello world");
+
+/*  un commentaire 
+	  plus long 
+		sur plusieurs lignes
+*/
+
+let messsage; // déclarer une variable
+message = 'Bonjour'; // assigner une valeur à cette variable
+
+
+let nombre = 42; // assigner une valeur directement à la déclaration
+
+
+let myMessageForTheWorld = 'Hello Franck';
+
+console.log(myMessageForTheWorld); // Affiche 'Hello World'
+
+
+// version avec une valeur par défaut
+
+let userAge = prompt("Quel est votre âge ?");
+console.log(userAge); // Affiche l'âge entré par l'utilisateur dans la console
